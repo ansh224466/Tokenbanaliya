e9763675b32d7b0961ed80026e408623a334735c
Miko756 <164617880+Miko756@users.noreply.github.com>
2024-07-11 22:11:25 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -151,7 +151,7 @@ async def start_command(client: Client, message: Message):
                     [InlineKeyboardButton("Click here", url=link)],
                     [InlineKeyboardButton('How to use the bot', url=full_tut_url)]
                 ]
-                await message.reply(f"Your Ads token is expired, refresh your token and try again.\n\nToken Timeout: {get_exp_time(VERIFY_EXPIRE)}\n\nWhat is the token?\n\nThis is an ads token. If you pass 1 ad, you can use the bot for 24 Hour after passing the ad.", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
+                await message.reply(f"**Baka Your Ads Token Is Expired, Start Bot Again And Take New Token.**\n\n**Token Timeout**: {get_exp_time(VERIFY_EXPIRE)}\n\n**What Is The Token?**\n\n**This Is An Ads Token. If You Pass 3 Page Ad, You Can Use The Bot For 24 Hour After Passing The Ad.**", reply_markup=InlineKeyboardMarkup(btn), protect_content=False, quote=True)
 
 # ... (rest of the code remains unchanged))
 
